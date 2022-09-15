792ea940c8a06fa5dbf729faa1a8d63455f97354
christianhan <christianhan@localhost>
2022-09-14 18:45:40 -0700

    Github Review day 2

---
+++ FridayProject/src/Driver.java
@@ -0,0 +1,15 @@
+
+public class Driver {
+
+	public static void main(String[] args) { //opening curly-- code below
+		
+		//this is the "starting point of the project
+		// code below will execute 
+		
+		System.out.println("Christian Han");
+		System.out.println("Happy Friday");
+		
+
+	} //closing curly - - add code above 
+
+}
---
+++ FridayProject/src/driver2.java
@@ -0,0 +1,71 @@
+
+public class driver2 {
+
+	public static void main(String[] args) {
+		
+		//primitive types - variables
+		//only stores one value at a time
+		/*
+		 * syntax:
+		 *  type name;
+		 */
+		int x; //declare a variable x to hold ints
+		
+		//assignment statement
+		x = 13;
+		
+//		print the value of x
+//		     System.out.println(x);
+//		    System.out.print(x);
+//		    System.out.print(x);
+//		    System.out.println(x);
+//		    System.out.print(x);
+//		    System.out.println(x);
+//		    System.out.println(x);
+		
+		//declare AND assign
+		int y = 5; //initializes the value at the same time
+		
+		//simple arithmetic in java
+		System.out.println(x + y);
+		
+		System.out.println(x +"+"+ y); //string + number - > String
+
+		
+		//find the product x multiplied to y 
+		System.out.println(x * y);
+		
+
+		//x divided by y?
+		System.out.println(x/y);
+		
+		//remainder - mod operator
+		//what is the remainder after dividing x by y?
+		System.out.println(7%15);
+		System.out.println(1%3000);
+		
+		
+		//x to the power of y
+		//there is no simple operator for this one!!!!!
+		System.out.print( Math.pow(x, y)    ); //there is a method for this 
+		
+		
+		
+		//Decimal values?
+		//double
+		int     myX = 3;
+		double  myY = 3.5;
+		
+		
+		//true or false values?
+		//boolean
+		boolean isFriday = true;
+		
+
+		
+	
+		
+
+
+	}
+}
